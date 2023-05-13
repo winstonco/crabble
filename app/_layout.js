630e3ae38e3d292335f3e9a3f7eedac25c02ac04
Winston Co <winstonco@live.com>
2023-05-13 11:17:21 -1000

    add icon and header tags

---
+++ app/_layout.js
@@ -0,0 +1,17 @@
+import { Slot } from 'expo-router';
+import Head from 'expo-router/head';
+
+const AppLayout = () => {
+  return (
+    <>
+      <Head>
+        <title>Crabble</title>
+        <meta name="description" content="A crab themed Scrabble bot" />
+        <link rel="icon" type="image/x-icon" href="/assets/favicon.png" />
+      </Head>
+      <Slot />
+    </>
+  );
+};
+
+export default AppLayout;
